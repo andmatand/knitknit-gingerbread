d201227295f64b56b4bf0ef0fb784f5d20e477ca
Andrew Anderson <andrewloveskitties@gmail.com>
2011-11-20 14:23:08 -0800

    Updated to r15 of android-sdk

--- src/com/example/knitknit/Settings.java
+++ src/com/example/knitknit/Settings.java
@@ -39,6 +39,7 @@ public class Settings extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
 	}
 
 	@Override
